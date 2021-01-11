73c6de65374558998f901796eab81531463ff11b
akshitaawasthi <55806504+akshitaawasthi@users.noreply.github.com>
2021-01-11 12:40:07 +0530

    Update query.js

--- query.js
+++ query.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
         // Transition effect for navbar 
         $(window).scroll(function() {
-          // checks if window is scrolled more than 500px, adds/removes solid class
-          if($(this).scrollTop() > 500) { 
+          // checks if window is scrolled more than 100px, adds/removes solid class
+          if($(this).scrollTop() > 100) { 
               $('.navbar').addClass('solid');
           } else {
               $('.navbar').removeClass('solid');
